70e632693aaab132e139774f98577acd116558cc
ajax333221 <myemailisepic@gmail.com>
2021-06-30 19:37:23 -0500

    upload files

---
+++ README.md
@@ -0,0 +1,45 @@
+<p align="center"><a href="https://github.com/ajax333221/isepic-chess"><img width="80" src="https://github.com/ajax333221/isepic-chess-ui/raw/master/css/images/ic_logo.png" alt="Ic.js logo"></a> <a href="https://github.com/ajax333221/isepic-chess-ui"><img width="80" src="https://github.com/ajax333221/isepic-chess-ui/raw/master/css/images/ic_ui_logo.png" alt="IcUi logo"></a></p>
+
+<h1 align="center">reddit-chess-userscript</h1>
+
+This userscript allows you to view **u/chessvision-ai-bot** position and continuation moves in a chessboard.
+
+It uses <a href="https://github.com/ajax333221/isepic-chess">Ic.js</a>, <a href="https://github.com/ajax333221/isepic-chess-ui">IcUi</a> and <a href="https://github.com/jquery/jquery">jQuery</a> as dependencies.
+
+:pushpin: Table of contents
+-------------
+
+- [reddit-chess-userscript](https://github.com/ajax333221/reddit-chess-userscript#reddit-chess-userscript)
+- [Table of contents](https://github.com/ajax333221/reddit-chess-userscript#pushpin-table-of-contents)
+- [How to use?](https://github.com/ajax333221/reddit-chess-userscript#computer_mouse-how-to-use)
+- [Features](https://github.com/ajax333221/reddit-chess-userscript#rocket-features)
+- [Copyright and license](https://github.com/ajax333221/reddit-chess-userscript#page_facing_up-copyright-and-license)
+
+:computer_mouse: How to use?
+-------------
+
+1. Install a userscript manager (e.g. Greasemonkey).
+
+2. Install the userscript `reddit-chess.user.js`.
+
+3. Browser reddit **r/chess** and look for **u/chessvision-ai-bot** comments.
+
+4. :warning:**Important:** due to a bug, when you visit a a post from the feed, you need to refresh the page or visit the URL directly for the script to work.
+
+:rocket: Features
+-------------
+
+- The board is playable
+- Load the solution (Best continuation)
+- Highlight last move, legal moves and checks
+- Navigation buttons
+- Rotate board
+- Promotion dropdown
+- Text with SAN moves (with jump-to-move links)
+
+:page_facing_up: Copyright and license
+-------------
+
+Copyright © 2021 Ajax Isepic (ajax333221)
+
+Licensed under MIT License: http://opensource.org/licenses/mit-license.php
---
+++ chessgames.user.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name           Chessgames IcUi replayer
+// @author         ajax333221
+// @description    It takes the plain-text PGN and turns it into a chess board
+// @version        0.7.0
+// @include        http://chessgames.com/*
+// @include        http://*.chessgames.com/*
+// @include        https://chessgames.com/*
+// @include        https://*.chessgames.com/*
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require        https://ajax333221.github.io/isepic-chess-ui/js/isepic-chess.js
+// @require        https://ajax333221.github.io/isepic-chess-ui/js/isepic-chess-ui.js
+// @icon           https://github.com/ajax333221/isepic-chess-ui/raw/master/css/images/ic_ui_logo.png
+// ==/UserScript==
+
+function mParamUrl(){
+	var results, rtn;
+	
+	rtn=0;
+	
+	results=new RegExp("[\?&]m=([^&#]*)").exec(window.location.href);
+	
+	if(results!=null){
+		rtn=(decodeURI(results[1]) || 0);
+	}
+	
+	return (rtn*1 || 0);
+}
+
+$(function(){
+	var i, len, arr, temp, mycss;
+	
+	$("<link>").appendTo("head").attr({
+		type: "text/css", 
+		rel: "stylesheet",
+		href: "https://ajax333221.github.io/isepic-chess-ui/css/isepic-chess-ui.css"
+	});
+	
+	temp=(""+$("table tt").first().html());
+	
+	if(Ic && IcUi && temp.length>10){
+		arr=["bp", "bn", "br", "bb", "bq", "bk", "wp", "wn", "wr", "wb", "wq", "wk"];
+		
+		mycss="<style type=\"text/css\">";
+		mycss+=".ic_ui_board .ic_piece_holder{background-repeat: no-repeat;background-position: 50% 50%;display:block;}";
+		
+		for(i=0, len=arr.length; i<len; i++){
+			mycss+=".ic_ui_board .ic_merida .ic_"+arr[i]+"{background-image: url(\"https://ajax333221.github.io/isepic-chess-ui/css/images/chess-fonts/merida/"+arr[i]+".png\");}";
+		}
+		
+		mycss+="</style>";
+		
+		$(mycss).appendTo("head");
+		
+		$("table tt").closest("td").css({
+			"background" : "#f0f6f7",
+			"padding" : "24px"
+		});
+		
+		$("table tt").first().html("<div class=\"ic_ui_board\" style=\"height:500px; width:500px;\"><div id=\"ic_ui_board\"></div></div><br><div class=\"ic_ui_controls\"><div id=\"icbuttons\"><input id=\"ic_ui_nav_first\" value=\"|<\" type=\"button\"><input id=\"ic_ui_nav_previous\" value=\"<\" type=\"button\"><input id=\"ic_ui_nav_next\" value=\">\" type=\"button\"><input id=\"ic_ui_nav_last\" value=\">|\" type=\"button\"><input id=\"ic_ui_rotate\" value=\"rotate\" type=\"button\"><select id=\"ic_ui_promote\"><option value=\"5\" selected=\"selected\">queen</option><option value=\"4\">rook</option><option value=\"3\">bishop</option><option value=\"2\">knight</option></select></div><br><input id=\"ic_ui_fen\" value=\"\" type=\"text\"><br><br><div class=\"ic_ui_move_list\"><div id=\"ic_ui_move_list\"></div></div></div>");
+		
+		$("#icbuttons").css("text-align", "center");
+		$("#icbuttons input").css({"margin" : "0 2px", "padding" : "8px 16px"});
+		$("#ic_ui_board").parent().css("margin", "0 auto");
+		
+		IcUi.setCfg("arrowKeysNavigation", true);
+		IcUi.setCfg("scrollNavigation", false);
+		IcUi.setCfg("soundEffects", false);
+		
+		Ic.initBoard({
+			boardName : "main",
+			moveIndex : Math.max(0, (((mParamUrl()-0.5)*2)-1)),
+			pgn : temp
+		});
+	}
+});
